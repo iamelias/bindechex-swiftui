94638c908813d3957d7bbbba0e0f5e97b1730423
Elias Hall <elias.hall@gmail.com>
2020-07-28 12:07:03 -0500

    Initial Commit

---
+++ BinDecHexSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BinDecHexSwiftUI
+//
+//  Created by Elias Hall on 7/28/20.
+//  Copyright © 2020 Elias Hall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
